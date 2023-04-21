a1abb971a77037ab797d07273025ba5061642d45
Nakano <hirota7771@rakuten.jp>
2023-04-22 07:31:08 +0900

    rivese

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import { Distance } from './component/Distance';
-import { GpsPosition } from './component/GpsPosition';
 import { TotalDistance } from './component/TotalDistance';
 import { ChakraProvider } from '@chakra-ui/react';
 
