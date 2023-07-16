22ebff47065270dd39dcd673490e58e2278475c9
Nakano <hirota7771@rakuten.jp>
2023-07-16 20:23:12 +0900

    revise component

--- src/component/DistanceCalculatorPopup.js
+++ src/component/DistanceCalculatorPopup.js
@@ -1,4 +1,6 @@
-import { Box, Button, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, ModalOverlay, useDisclosure } from "@chakra-ui/react"
+import { Box, Button, HStack, IconButton, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, ModalOverlay, VStack, useDisclosure } from "@chakra-ui/react"
+import { DistanceCalculator } from "./DistanceCalculator";
+import {BsX} from "react-icons/bs";
 
 export const DistanceCalculatorPopup = () => {
     const {isOpen, onOpen, onClose} = useDisclosure();
@@ -15,11 +17,21 @@ export const DistanceCalculatorPopup = () => {
             
             <ModalContent>
                 <ModalHeader>
-
+                    <Box textAlign = {"right"}>
+                        <IconButton
+                            icon = {<BsX size={"25px"}/>}
+                            variant = {"ghost"}
+                            onClick = {onClose}
+                        />
+                    </Box>
                 </ModalHeader>
 
                 <ModalBody>
-                    
+                    <VStack>
+                        <HStack>
+                            <DistanceCalculator />
+                        </HStack>
+                    </VStack>
                 </ModalBody>
 
                 <ModalFooter>
